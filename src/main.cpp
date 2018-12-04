b0a2024c3246980dd298b5f04e413d1489300a1f
Nat <nat@cmmc.io>
2018-12-04 22:04:04 +0700

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+static const uint32_t GPSBaud = 9600;
+
+#include <TinyGPS++.h>
+TinyGPSPlus gps;
+// HardwareSerial mySerial1(2);
+void processGps();
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(9600, SERIAL_8N1, 22 /*rx*/, 21 /* tx */);
+}
+
+void loop() { //Choose Serial1 or Serial2 as required
+  processGps();
+}
+
+void processGps() {
+  while (Serial1.available() > 0) {
+    char c = Serial1.read();
+    if (gps.encode(c)) {
+      if (gps.location.isValid()) {
+        Serial.print(gps.location.lat(), 6);
+        Serial.print(F(","));
+        Serial.print(gps.location.lng(), 6);
+        RawDegrees lat = gps.location.rawLat();
+        RawDegrees lng = gps.location.rawLng();
+        char latC[20];
+        char lngC[20];
+        sprintf(latC, "%c%u.%lu", lat.negative?'-':' ', lat.deg, lat.billionths);
+        sprintf(lngC, "%c%u.%lu", lng.negative?'-':' ', lng.deg, lng.billionths);
+        Serial.printf("%s,%s\r\n", latC, lngC);
+      }
+      else
+      {
+        // Serial.println(F("INVALID"));
+      }
+    }
+  }
+}
